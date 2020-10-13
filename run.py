bf3bf72d9cdb58b8f9ff376870124f6c40a5bce3
kiptoo-097 <kiptoo097.com>
2020-10-14 00:34:14 +0300

    added delete user and save user function

--- run.py
+++ run.py
@@ -7,3 +7,15 @@ def create_user(name, user_password):
     """
     created_user = User(name, user_password)
     return created_user
+
+def save_user(user):
+    """
+    Function to save user
+    """
+    user.save_user()
+
+def delete_user(user):
+    """
+    delete user function
+    """
+    user.delete_user()
