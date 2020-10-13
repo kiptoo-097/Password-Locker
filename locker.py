7bbcf545b5fd5b1b7b382d55a78d441ccd662139
kiptoo-097 <kiptoo097.com>
2020-10-13 23:19:27 +0300

    created class save_ user

--- locker.py
+++ locker.py
@@ -4,8 +4,15 @@ class User:
     """
     user_list = []
 
+    def __init__(self, name, user_password):
+        self.name = name
+        self.user_password = user_password
 
-
+    def save_user(self):
+        """
+        save_user method saves user objects into user_list
+        """
+        User.user_list.append(self)
 
 
 
