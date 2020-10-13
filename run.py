2d710a3e0dc04ef3e29cfd7caff0cc3df6ee05f9
kiptoo-097 <kiptoo097.com>
2020-10-14 01:13:17 +0300

    corrected errors on parameters

--- run.py
+++ run.py
@@ -19,3 +19,52 @@ def delete_user(user):
     delete user function
     """
     user.delete_user()
+
+
+def create_credential(account, acnt_username, acnt_password):
+    """
+    Parameters
+    """
+    new_credential = Credential(account, acnt_username, acnt_password)
+    return new_credential
+
+def save_credential(credential):
+    """
+    Function to save credential
+    """
+    credential.save_credential()
+
+
+def delete_credential(credential):
+    """
+    Function to delete credential
+    """
+    credential.delete_credential()
+
+
+def find_credential(acnt_username):
+    """
+    Function to find credential
+    """
+    return Credential.find_by_acnt_username(acnt_username)
+
+
+def check_existing_credentials(acnt_username):
+    """
+    Function to check existing credential
+    ----------
+    name
+    Returns
+    -------
+    user
+    """
+    return Credential.find_by_acnt_username(acnt_username)
+
+
+def display_credentials():
+    """
+    Function to display credential
+    Returns
+    -------
+    """
+    return Credential.display_all_credentials()
